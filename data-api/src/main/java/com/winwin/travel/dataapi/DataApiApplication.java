1b47e128bc95034bd3a56acb4b4c43839bd4e505
Nazir Savysko <nazirsavisko@gmail.com>
2026-02-28 22:05:18 +0200

    Clean up imports in DataApiApplication class

--- data-api/src/main/java/com/winwin/travel/dataapi/DataApiApplication.java
+++ data-api/src/main/java/com/winwin/travel/dataapi/DataApiApplication.java
@@ -1,8 +1,7 @@
 package com.winwin.travel.dataapi;
 
-import org.apache.catalina.filters.HttpHeaderSecurityFilter;import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DataApiApplication {
